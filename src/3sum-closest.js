ea6c80b84cfc49f99b3afaf89bf588373fb19d64
chnbohwr <chnbohwr@gmail.com>
2019-10-30 17:46:38 +0800

    add threeSumClosest

---
+++ src/3sum-closest.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+  let answer = Infinity;
+  let answerDiff = Infinity;
+  if (nums.length < 3) {
+    return answer;
+  }
+  nums = nums.sort((a, b) => (a - b));
+  const maxCursor = nums.length - 1;
+  for (let a = 0; a <= maxCursor; a++) {
+    if (a > 0 && nums[a] === nums[a - 1]) {
+      continue;
+    }
+    let b = a + 1, c = maxCursor;
+    while (b < c) {
+      const numa = nums[a];
+      const numb = nums[b];
+      const numc = nums[c];
+      const total = numa + numb + numc;
+      const diff = Math.abs(target - total);
+      if (diff < answerDiff) {
+        answerDiff = diff;
+        answer = total;
+      }
+      if (total === target) {
+        console.log('find real answer', numa, numb, numc);
+        return target;
+      } else if (total > target) {
+        c--;
+      } else {
+        b++
+      }
+    }
+  }
+  return answer;
+};
+
+module.exports = threeSumClosest;
---
+++ test/3sum-closest.test.js
@@ -0,0 +1,10 @@
+const func = require('../src/3sum-closest');
+
+describe('3sum-closest', () => {
+  test('should ', () => {
+    expect(func([-1, 2, 1, -4], 1)).toBe(2)
+  });
+  test('should ', () => {
+    expect(func([1, 1, 1, 1], 0)).toBe(3)
+  });
+});
